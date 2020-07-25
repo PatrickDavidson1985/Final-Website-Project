d26bd472a5c063435f4f73a259036546c42d5210
Patrick Davidson <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-07-24 20:51:19 -0400

    javascript validation

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var x = document.forms["myForm"]["fname"]["email"]["address"]["city"]["state"]["zipCode"].value;
+    var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
       alert("all fields must be filled out");
       return false;
@@ -8,9 +8,49 @@ function validateForm() {
 
 
   function validateForm() {
-    var x = document.forms["myForm"]["lname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
+    var y = document.forms["myForm"]["lname"].value;
+    if (y == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var z = document.forms["myForm"]["email"].value;
+    if (z == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var xx = document.forms["myForm"]["address"].value;
+    if (xx == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var a = document.forms["myForm"]["city"].value;
+    if (a == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var b = document.forms["myForm"]["state"].value;
+    if (b == "") {
+      alert("all fields must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var c = document.forms["myForm"]["zipCode"].value;
+    if (c == "") {
+      alert("all fields must be filled out");
       return false;
     }
   }
